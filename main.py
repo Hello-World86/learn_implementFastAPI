54ed9acfe9ccc600fe366b364b5144a2e334ced1
Hello-World86 <luniva086@gmail.com>
2026-04-05 17:18:47 +0545

    Implement path and query params

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException, Query
 import json 
 
 app = FastAPI()
 
 def load_data():
     try:
-        with open('patient_info.json', 'r') as f:
+        with open('patient.json', 'r') as f:
             return json.load(f)
     except FileNotFoundError:
         return {"error": "patient_info.json was not found"}
@@ -25,3 +25,34 @@ def about():
 def view():
     data = load_data()
     return data
+
+#Implementation of conceot of Path parameter to provide document example
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str = Path(..., description='ID of the patient in the DB',
+example = 'P001')):
+    #load all the patient's data at first
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code = 404, detail ='Patient doesnot exist in the system.')
+#Use required as well as default query parameter to sort patient info 
+@app.get('/sort')
+def sort_patient_info(sort_by: str= Query(..., description='Sort on the basis of height, weight or bmi'), 
+order: str = Query('asc', description = 'sort either in an ascending or descending order')):
+    
+    valid_fields = ['height', 'weight', 'bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code = 400, detail=f'Invalid field selected from {valid_fields}')
+
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=400, detail='Invalid order select between asc and desc')
+    
+    data = load_data()
+
+    sort_order = True if order=='desc' else False
+
+    sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse= sort_order)
+
+    return sorted_data
